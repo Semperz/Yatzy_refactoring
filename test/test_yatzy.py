969067bc26fc44e9106075bdfe6c52054b9ae61c
a23sergiopg <serprigar@gmail.com>
2024-01-15 08:38:28 +0100

    test(test_yatzy.py): add pytest markers to test functions

--- test/test_yatzy.py
+++ test/test_yatzy.py
@@ -1,13 +1,14 @@
 from src.yatzy import Yatzy
+import pytest
 
-
+@pytest.mark.chance
 def test_chance_all_dice_sum():
     expected = 15
     actual = Yatzy.chance(2, 3, 4, 5, 1)
     assert expected == actual
     assert 16 == Yatzy.chance(3, 3, 4, 5, 1)
 
-
+@pytest.mark.yatzy
 def test_yatzy_scores_50():
     expected = 50
     actual = Yatzy.yatzy(4, 4, 4, 4, 4)
@@ -15,79 +16,79 @@ def test_yatzy_scores_50():
     assert 50 == Yatzy.yatzy(6, 6, 6, 6, 6)
     assert 0 == Yatzy.yatzy(6, 6, 6, 6, 3)
 
-
+@pytest.mark.ones
 def test_ones():
     assert Yatzy.ones(1, 2, 3, 4, 5) == 1
     assert 2 == Yatzy.ones(1, 2, 1, 4, 5)
     assert 0 == Yatzy.ones(6, 2, 2, 4, 5)
     assert 4 == Yatzy.ones(1, 2, 1, 1, 1)
 
-
+@pytest.mark.twos
 def test_twos():
     assert 4 == Yatzy.twos(1, 2, 3, 2, 6)
     assert 10 == Yatzy.twos(2, 2, 2, 2, 2)
 
-
+@pytest.mark.threes
 def test_threes():
     assert 6 == Yatzy.threes(1, 2, 3, 2, 3)
     assert 12 == Yatzy.threes(2, 3, 3, 3, 3)
 
-
+@pytest.mark.fours
 def test_fours():
     assert 12 == Yatzy.fours(4, 4, 4, 5, 5)
     assert 8 == Yatzy.fours(4, 4, 5, 5, 5)
     assert 4 == Yatzy.fours(4, 5, 5, 5, 5)
 
-
+@pytest.mark.fives
 def test_fives():
     assert 10 == Yatzy.fives(4, 4, 4, 5, 5)
     assert 15 == Yatzy.fives(4, 4, 5, 5, 5)
     assert 20 == Yatzy.fives(4, 5, 5, 5, 5)
 
-
+@pytest.mark.sixes
 def test_sixes():
     assert 0 == Yatzy.sixes(4, 4, 4, 5, 5)
     assert 6 == Yatzy.sixes(4, 4, 6, 5, 5)
     assert 18 == Yatzy.sixes(6, 5, 6, 6, 5)
 
-
+@pytest.mark.pair
 def test_one_pair():
     assert 6 == Yatzy.score_pair(3, 4, 3, 5, 6)
     assert 10 == Yatzy.score_pair(5, 3, 3, 3, 5)
     assert 12 == Yatzy.score_pair(5, 3, 6, 6, 5)
 
-
+@pytest.mark.pairs
 def test_two_pair():
     assert 16 == Yatzy.two_pair(3, 3, 5, 4, 5)
     assert 18 == Yatzy.two_pair(3, 3, 6, 6, 6)
     assert 0 == Yatzy.two_pair(3, 3, 6, 5, 4)
 
-
+@pytest.mark.three_kind
 def test_three_of_a_kind():
     assert 9 == Yatzy.three_of_a_kind(3, 3, 3, 4, 5)
     assert 15 == Yatzy.three_of_a_kind(5, 3, 5, 4, 5)
     assert 9 == Yatzy.three_of_a_kind(3, 3, 3, 3, 5)
 
-
+@pytest.mark.four_kind
 def test_four_of_a_kind():
     assert 12 == Yatzy.four_of_a_kind(3, 3, 3, 3, 5)
     assert 20 == Yatzy.four_of_a_kind(5, 5, 5, 4, 5)
     assert 12 == Yatzy.four_of_a_kind(3, 3, 3, 3, 3)
     assert 0 == Yatzy.four_of_a_kind(3, 3, 3, 2, 1)
 
-
+@pytest.mark.small
 def test_small_straight():
     assert 15 == Yatzy.small_straight(1, 2, 3, 4, 5)
     assert 15 == Yatzy.small_straight(2, 3, 4, 5, 1)
     assert 0 == Yatzy.small_straight(1, 2, 2, 4, 5)
 
-
+@pytest.mark.large
 def test_large_straight():
     assert 20 == Yatzy.large_straight(6, 2, 3, 4, 5)
     assert 20 == Yatzy.large_straight(2, 3, 4, 5, 6)
     assert 0 == Yatzy.large_straight(1, 2, 2, 4, 5)
 
-
+@pytest.mark.full_house
 def test_full_house():
     assert 18 == Yatzy.full_house(6, 2, 2, 2, 6)
     assert 0 == Yatzy.full_house(2, 3, 4, 5, 6)
