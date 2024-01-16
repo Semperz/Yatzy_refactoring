07c6b18a65905d66a8f102c07663204baec09aa2
a23sergiopg <serprigar@gmail.com>
2024-01-16 12:56:41 +0100

    refactor(yatzy.py): add optimization method to full house function and before refactoring code smells comments

--- src/yatzy.py
+++ src/yatzy.py
@@ -2,11 +2,17 @@ class Yatzy:
 
     @staticmethod
     def chance(*dices):
+        '''
+        Before refactor: Code is duplicated and function takes too many parameters
+        '''
         total = sum(dices)
         return total
 
     @staticmethod
     def yatzy(*dices):
+        '''
+        Before refactor: A loop is too long and too deeply nested
+        '''
         if dices.count(dices[0]) != len(dices):
             return 0
         else:
@@ -14,6 +20,9 @@ class Yatzy:
 
     @staticmethod
     def ones(*dices):
+        '''
+        Before refactor: Code is duplicated and function takes too many parameters
+        '''
         ones_sum = 0
         for ones in dices:
             if ones == 1:
@@ -22,6 +31,9 @@ class Yatzy:
 
     @staticmethod
     def twos(*dices):
+        '''
+        Before refactor: Code is duplicated and function takes too many parameters
+        '''
         twos_sum = 0
         for twos in dices:
             if twos == 2:
@@ -29,6 +41,9 @@ class Yatzy:
         return twos_sum
     @staticmethod
     def threes(*dices):
+        '''
+        Before refactor: Code is duplicated and function takes too many parameters
+        '''
         threes_sum = 0
         for threes in dices:
             if threes == 3:
@@ -36,6 +51,9 @@ class Yatzy:
         return threes_sum
     @staticmethod
     def fours(*dices):
+        '''
+        Before refactor: A routine uses more features of another class than of its own class
+        '''
         fours_sum = 0
         for fours in dices:
             if fours == 4:
@@ -44,6 +62,9 @@ class Yatzy:
 
     @staticmethod
     def fives(*dices):
+        '''
+        Before refactor: A routine uses more features of another class than of its own class
+        '''
         fives_sum = 0
         for fives in dices:
             if fives == 5:
@@ -52,6 +73,9 @@ class Yatzy:
 
     @staticmethod
     def sixes(*dices):
+        '''
+        Before refactor: A routine uses more features of another class than of its own class
+        '''
         sixes_sum = 0
         for sixes in dices:
             if sixes == 6:
@@ -60,6 +84,10 @@ class Yatzy:
 
     @staticmethod
     def score_pair(*dices):
+        '''
+        Before refactor: A chain of routines passes tramp data
+                         Code is duplicated and function takes too many parameters
+        '''
         for pairs in range(6,0,-1):
             if dices.count(pairs) >= 2:
                 return pairs * 2
@@ -68,6 +96,10 @@ class Yatzy:
 
     @staticmethod
     def two_pair(*dices):
+        '''
+        Before refactor: A chain of routines passes tramp data
+                         Code is duplicated and function takes too many parameters
+        '''
         double_pair = set()
         for pairs in range(6, 0, -1):
             double_pair_count = dices.count(pairs)
@@ -80,6 +112,10 @@ class Yatzy:
 
     @staticmethod
     def four_of_a_kind(*dices):
+        '''
+        Before refactor: A chain of routines passes tramp data
+                         Code is duplicated and function takes too many parameters
+        '''
         for num_fours in range(6, 0, -1):
             if dices.count(num_fours) >= 4:
                 return num_fours * 4
@@ -87,6 +123,10 @@ class Yatzy:
 
     @staticmethod
     def three_of_a_kind(*dices):
+        '''
+        Before refactor: A chain of routines passes tramp data
+                         Code is duplicated and function takes too many parameters
+        '''
         for num_threes in range(6, 0, -1):
             if dices.count(num_threes) >= 3:
                 return num_threes * 3
@@ -94,21 +134,34 @@ class Yatzy:
 
     @staticmethod
     def small_straight(*dices):
+        '''
+        Before refactor: A chain of routines passes tramp data
+                         Code is duplicated and function takes too many parameters
+        '''
         if tuple(sorted(dices)) == (1,2,3,4,5):
             return 15
         return 0
 
     @staticmethod
     def large_straight(*dices):
+        '''
+        Before refactor: A chain of routines passes tramp data
+                         Code is duplicated and function takes too many parameters
+        '''
         if tuple(sorted(dices)) == (2,3,4,5,6):
             return 20
         return 0
     @staticmethod
     def full_house(*dices):
+        '''
+        Before refactor: A loop is too long or too deeply nested
+                         Function takes too many parameters
+        '''
         dictionary = {}
         final_score = 0
         for number in dices:
-            dictionary[number] = dices.count(number)
+            if number not in dictionary:
+                dictionary[number] = dices.count(number)
         if len(dictionary) != 2:
             return 0
         if 2 in dictionary.values() and 3 in dictionary.values():
